bd8120450613efd487fc83b1b4c84146f2804482
RedWolf625 <55816376+RedWolf625@users.noreply.github.com>
2020-08-04 16:55:52 -0700

    Update giftCards.js

--- giftCards.js
+++ giftCards.js
@@ -1,37 +1,36 @@
 module.exports = function(){
     var express = require('express');
     var router = express.Router();
-    
-    function getGiftCards(res, mysql, context, complete){
-        mysql.pool.query("SELECT name, quantity, price, giftCardID FROM GiftCards", function(error, results, fields){
+
+
+
+    function getPeople(res, mysql, context, complete){
+        mysql.pool.query("SELECT name, quantity FROM GiftCards", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.giftCards = results;
+            context.people = results;
             complete();
         });
     }
-    
-     /*Adds a giftcard*/
-    router.post('/', function(req, res){
-        console.log(req.body.homeworld)
-        console.log(req.body)
+    /*Display all people. Requires web based javascript to delete users with AJAX*/
+
+    router.get('/', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO GiftCards (name, quantity, price) VALUES (?,?,?)";
-        var inserts = [req.body.name, req.body.quantity, req.body.price];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.redirect('/giftCards');
+        getPeople(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('giftcards', context);
             }
-        });
+
+        }
     });
 
-    
-    
-  return router;
-}(); 
+
+    return router;
+}();
