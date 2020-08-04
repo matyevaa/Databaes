225533cda52fef89bdd23d85b45867ceb22f1885
RedWolf625 <55816376+RedWolf625@users.noreply.github.com>
2020-08-04 16:57:38 -0700

    Update 340_setup.js

--- 340_setup.js
+++ 340_setup.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_shelbyn',
-  password       : 'Sp!tfire1',
-  database       : 'cs340_shelbyn'
+  user            : 'cs340_username',
+  password       : 'your password',
+  database       : 'cs340_username'
 });
 
 module.exports.pool = pool;
