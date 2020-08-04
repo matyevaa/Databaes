3c8b011ee56ff227a847210bae260ac491722b9b
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-04 13:44:23 -0700

    Create giftCards.js

---
+++ giftCards.js
@@ -0,0 +1,17 @@
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+    
+    function getGiftCards(res, mysql, context, complete){
+        mysql.pool.query("SELECT name, quantity, price, giftCardID FROM GiftCards", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.giftCards = results;
+            complete();
+        });
+    }
+    
+  return router;
+}();
