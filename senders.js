e65beb1c85cf8281d54f7f6e4c426b05d529961b
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-04 14:58:16 -0700

    Update senders.js

--- senders.js
+++ senders.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
     
     function getSenders(res, mysql, context, complete){
-        mysql.pool.query("SELECT ????", function(error, results, fields){
+        mysql.pool.query("SELECT OrderHistory.sender_email as sender_email, GiftCards.name as name, OrderHistory.quantity as quantity FROM OrderHistory LEFT JOIN senders ON senders.sender_email = OrderHistory.sender_email LEFT JOIN orders ON orders.orderID = OrderHistory.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
