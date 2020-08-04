dec7c0bc52d046384a9b0937cc014ae412fad0d8
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-04 16:06:42 -0700

    Update giftCards.js

--- giftCards.js
+++ giftCards.js
@@ -13,5 +13,25 @@ module.exports = function(){
         });
     }
     
+     /*Adds a giftcard*/
+    router.post('/', function(req, res){
+        console.log(req.body.homeworld)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO GiftCards (name, quantity, price) VALUES (?,?,?)";
+        var inserts = [req.body.name, req.body.quantity, req.body.price];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/giftCards');
+            }
+        });
+    });
+
+    
+    
   return router;
 }(); 
