1c39421bea90dd9fc6e191d7000e846dac1f324e
RedWolf625 <55816376+RedWolf625@users.noreply.github.com>
2020-08-04 16:53:24 -0700

    Update senders.js

--- senders.js
+++ senders.js
@@ -1,18 +1,36 @@
 module.exports = function(){
     var express = require('express');
     var router = express.Router();
-    
-    function getSenders(res, mysql, context, complete){
-        mysql.pool.query("SELECT OrderHistory.sender_email as sender_email, GiftCards.name as name, OrderHistory.quantity as quantity FROM OrderHistory LEFT JOIN senders ON senders.sender_email = OrderHistory.sender_email LEFT JOIN orders ON orders.orderID = OrderHistory.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID", function(error, results, fields){
+
+
+
+    function getPeople(res, mysql, context, complete){
+        mysql.pool.query("SELECT senders.email, GiftCards.name, orders.amount FROM orders LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN senders ON senders.email = OrderHistory.email", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.senders = results;
+            context.people = results;
             complete();
         });
     }
-    
-  return router;
-}();
+    /*Display all people. Requires web based javascript to delete users with AJAX*/
 
+    router.get('/', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        var mysql = req.app.get('mysql');
+        getPeople(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('senders', context);
+            }
+
+        }
+    });
+
+
+    return router;
+}();
