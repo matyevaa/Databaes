5111cc5cd2cf39a42840facaeb85958bdf8b22db
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-04 14:24:01 -0700

    Update recipients.js

--- recipients.js
+++ recipients.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
     
     function getRecipients(res, mysql, context, complete){
-        mysql.pool.query("SELECT orders.recipient_email, GiftCards.name, orders.quantity FROM orders LEFT JOIN recipients ON recipients.recipient_email = orders.recipient_email LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID", function(error, results, fields){
+        mysql.pool.query("SELECT orders.recipient_email as recipient_email, GiftCards.name as name, orders.quantity as quantity FROM orders LEFT JOIN recipients ON recipients.recipient_email = orders.recipient_email LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
