33097883e8e50ca21d77a0fd42ad88ab9c606893
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-04 14:23:24 -0700

    Update orderHistory.js

--- orderHistory.js
+++ orderHistory.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
     
     function getOrderHistory(res, mysql, context, complete){
-        mysql.pool.query("SELECT OrderHistory.orderID, orders.trackerID, OrderHistory.sender_email, orders.recipient_email, OrderHistory.quantity, GiftCards.name FROM OrderHistory LEFT JOIN senders ON OrderHistory.sender_email = senders.sender_email LEFT JOIN orders ON orders.orderID = OrderHistory.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID", function(error, results, fields){
+        mysql.pool.query("SELECT OrderHistory.orderID as orderID, orders.trackerID as trackerID, OrderHistory.sender_email as sender_email, orders.recipient_email as recipient_email, OrderHistory.quantity as quantity, GiftCards.name as name FROM OrderHistory LEFT JOIN senders ON OrderHistory.sender_email = senders.sender_email LEFT JOIN orders ON orders.orderID = OrderHistory.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
