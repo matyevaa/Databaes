97d2f3344f265343f3c29b8854ccd31d99688e1e
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-05 14:28:13 -0700

    Update recipients.js

--- recipients.js
+++ recipients.js
@@ -2,8 +2,6 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-
-
     function getRecipients(res, mysql, context, complete){
         mysql.pool.query("SELECT OrderHistory.orderID, orders.trackerID, orders.recipient_email as email, GiftCards.name, orders.price as amount FROM orders LEFT JOIN recipients ON orders.recipient_email = recipients.recipient_email LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID", function(error, results, fields){
             if(error){
@@ -30,23 +28,63 @@ module.exports = function(){
         }
     });
 
-    /*Adds a recipient, redirects to the recipients page after adding */
+     /*Adds a recipient, redirects to the recipients page after adding */
     router.post('/', function(req, res){
-        console.log(req.body.homeworld)
         console.log(req.body)
         var mysql = req.app.get('mysql');
-        var sql = /*"INSERT INTO recipients () VALUES (?)"*/;
-        var inserts = [req.body.orderID, req.body.email, req.body.name, req.body.amount];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+        
+        var email = req.body.email;
+        var name = req.body.name;
+        var amount = req.body.amount;
+        var orderID = req.body.orderID;
+        
+        var sql = "INSERT IGNORE INTO `senders` (`sender_email`) VALUES (?);";
+        var sql2 = "INSERT IGNORE INTO `GiftCards` (`name`) VALUES (?);";
+        var sql3 = "INSERT IGNORE INTO `orders` (`price`) VALUES (?);";
+        var sql4 = "INSERT IGNORE INTO `OrderHistory` (`orderID`) VALUES (?)";
+        
+        var inserts = [email];
+        var inserts2 = [name];
+        var inserts3 = [amount];
+        var inserts4 = [orderID];
+        
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){    
             if(error){
                 console.log(JSON.stringify(error))
                 res.write(JSON.stringify(error));
                 res.end();
             }else{
-                res.redirect('/recipients');
             }
         });
+        
+        sql2 = mysql.pool.query(sql2,inserts2,function(error, results, fields){    
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+            }
+        });
+        
+        sql3 = mysql.pool.query(sql3,inserts3,function(error, results, fields){    
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+            }
+        });
+        
+        sql4 = mysql.pool.query(sql4,inserts4,function(error, results, fields){    
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+              res.redirect('/recipients');
+            }
+        });    
     });
-
+    
     return router;
 }();
