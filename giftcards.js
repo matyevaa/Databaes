db714c2b0d07d45c56728ecab209e34584ff062d
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-05 11:39:39 -0700

    Update giftcards.js

--- giftcards.js
+++ giftcards.js
@@ -2,26 +2,24 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-
-
-    function getPeople(res, mysql, context, complete){
+    function getGiftCards(res, mysql, context, complete){
         mysql.pool.query("SELECT name, quantity FROM GiftCards", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.people = results;
+            context.giftcards = results;
             complete();
         });
     }
-    /*Display all people. Requires web based javascript to delete users with AJAX*/
 
+    //display giftcards
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
         context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
+        getGiftCards(res, mysql, context, complete);
         function complete(){
             callbackCount++;
             if(callbackCount >= 1){
@@ -29,6 +27,24 @@ module.exports = function(){
             }
 
         }
+    });
+    
+  /* Adds a giftcard, redirects to the GiftCards page after adding */
+    router.post('/', function(req, res){
+        console.log(req.body.homeworld)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO GiftCards (name, quantity) VALUES (?,?)";
+        var inserts = [req.body.name, req.body.amount];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/giftcards');
+            }
+        });
     });
 
 
