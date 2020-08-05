ba364a7a4c56d0f952fd153193055e6a712c22cf
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-05 12:43:59 -0700

    Update recipients.js

--- recipients.js
+++ recipients.js
@@ -4,34 +4,49 @@ module.exports = function(){
 
 
 
-    function getPeople(res, mysql, context, complete){
+    function getRecipients(res, mysql, context, complete){
         mysql.pool.query("SELECT OrderHistory.orderID, orders.trackerID, orders.recipient_email as email, GiftCards.name, orders.price as amount FROM orders LEFT JOIN recipients ON orders.recipient_email = recipients.recipient_email LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.people = results;
+            context.recipients = results;
             complete();
         });
     }
 
-    /*Display all people. Requires web based javascript to delete users with AJAX*/
-
+    /*Display all recipients. Requires web based javascript to delete recipients with AJAX*/
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
         context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
+        getRecipients(res, mysql, context, complete);
         function complete(){
             callbackCount++;
             if(callbackCount >= 1){
                 res.render('recipients', context);
             }
-
         }
     });
 
+    /*Adds a recipient, redirects to the recipients page after adding */
+    router.post('/', function(req, res){
+        console.log(req.body.homeworld)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = /*"INSERT INTO recipients () VALUES (?)"*/;
+        var inserts = [req.body.orderID, req.body.email, req.body.name, req.body.amount];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/recipients');
+            }
+        });
+    });
 
     return router;
 }();
