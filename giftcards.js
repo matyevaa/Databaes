5c72d5a4798579ae443abb8aabceb9c5ccb76ed6
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-05 15:18:00 -0700

    Update giftcards.js

--- giftcards.js
+++ giftcards.js
@@ -3,12 +3,25 @@ module.exports = function(){
     var router = express.Router();
 
     function getGiftCards(res, mysql, context, complete){
-        mysql.pool.query("SELECT name, quantity FROM GiftCards", function(error, results, fields){
+        mysql.pool.query("SELECT giftCardID as id, name, quantity FROM GiftCards", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
             context.giftcards = results;
+            complete();
+        });
+    }
+    
+    function getGiftCard(res, mysql, context, id, complete){
+        var sql = "SELECT giftCardID as id, name, quantity FROM GiftCards WHERE giftCardID=?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.giftcard = results[0];
             complete();
         });
     }
@@ -31,11 +44,10 @@ module.exports = function(){
     
   /* Adds a giftcard, redirects to the GiftCards page after adding */
     router.post('/', function(req, res){
-        console.log(req.body.homeworld)
         console.log(req.body)
         var mysql = req.app.get('mysql');
         var sql = "INSERT INTO GiftCards (name, quantity) VALUES (?,?)";
-        var inserts = [req.body.name, req.body.amount];
+        var inserts = [req.body.name, req.body.quantity];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(JSON.stringify(error))
@@ -46,7 +58,42 @@ module.exports = function(){
             }
         });
     });
+    
+    /* Display one giftcard for the specific purpose of updating giftcards*/
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["updategiftcard.js"];
+        var mysql = req.app.get('mysql');
+        getGiftCard(res, mysql, context, req.params.id, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('update-giftcard', context);
+            }
 
+        }
+    });
+    
+    /* The URI that update data is sent to in order to update a giftcard */
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        console.log(req.body)
+        console.log(req.params.id)
+        var sql = "UPDATE GiftCards SET name=?, quantity=? WHERE giftCardID=?";
+        var inserts = [req.body.name, req.body.quantity, req.params.id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+    
 
     return router;
 }();
