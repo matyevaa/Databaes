330c5dad6766e5213d6308c4ce193d5d0f12457d
RedWolf625 <55816376+RedWolf625@users.noreply.github.com>
2020-08-05 13:36:02 -0700

    Update orders.js

--- orders.js
+++ orders.js
@@ -4,8 +4,19 @@ module.exports = function(){
 
 
 
-    function getPeople(res, mysql, context, complete){
+    function getOrders(res, mysql, context, complete){
         mysql.pool.query("SELECT OrderHistory.orderID, orders.trackerID, OrderHistory.sender_email as sender_email, orders.recipient_email as recipient_email, GiftCards.name, orders.price as amount FROM orders LEFT JOIN OrderHistory ON orders.orderID = OrderHistory.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.people = results;
+            complete();
+        });
+    }
+
+    function insertOrderHist(sEmail, res, mysql, context, complete){
+        mysql.pool.query("", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -22,7 +33,7 @@ module.exports = function(){
         var context = {};
         context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
+        getOrders(res, mysql, context, complete);
         function complete(){
             callbackCount++;
             if(callbackCount >= 1){
@@ -32,6 +43,35 @@ module.exports = function(){
         }
     });
 
+    router.post('/', function(req, res){
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sEmail = req.body.sEmail;
+        var rEmail = req.body.rEmail;
+        var giftCard = req.body.giftCard;
+        var amount = req.body.amount;
+        var sql = "INSERT IGNORE INTO `senders` (`sender_email`) VALUES (?);";
+        var sql2 = "INSERT IGNORE INTO `recipients` (`recipient_email`) VALUES (?)";
+        var inserts = [sEmail];
+        var inserts2 = [rEmail];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+            }
+        });
+        sql2 = mysql.pool.query(sql2,inserts2,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/orders');
+            }
+        });
+    });
 
     return router;
 }();
