9dbd83818693361eee65f0395bdaeda7c8336f61
RedWolf625 <55816376+RedWolf625@users.noreply.github.com>
2020-08-04 17:31:18 -0700

    Update senders.js

--- senders.js
+++ senders.js
@@ -5,7 +5,7 @@ module.exports = function(){
 
 
     function getPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT senders.sender_email, GiftCards.name, orders.quantity FROM orders LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN senders ON senders.sender_email = OrderHistory.sender_email", function(error, results, fields){
+        mysql.pool.query("SELECT senders.sender_email, GiftCards.name, orders.price FROM orders LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN senders ON senders.sender_email = OrderHistory.sender_email", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
