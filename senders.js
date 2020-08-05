e013840e1e0df63183ae1f5138bfd73467093abb
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-05 12:44:34 -0700

    Update senders.js

--- senders.js
+++ senders.js
@@ -3,14 +3,13 @@ module.exports = function(){
     var router = express.Router();
 
 
-
-    function getPeople(res, mysql, context, complete){
+    function getSenders(res, mysql, context, complete){
         mysql.pool.query("SELECT senders.sender_email as email, GiftCards.name, orders.price as amount FROM orders LEFT JOIN OrderHistory ON OrderHistory.orderID = orders.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID LEFT JOIN senders ON senders.sender_email = OrderHistory.sender_email", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.people = results;
+            context.senders = results;
             complete();
         });
     }
@@ -21,7 +20,7 @@ module.exports = function(){
         var context = {};
         context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
+        getSenders(res, mysql, context, complete);
         function complete(){
             callbackCount++;
             if(callbackCount >= 1){
@@ -29,6 +28,24 @@ module.exports = function(){
             }
 
         }
+    });
+        
+      /*Adds a sender, redirects to the senders page after adding */
+    router.post('/', function(req, res){
+        console.log(req.body.homeworld)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = /*"INSERT INTO senders (sender_email) VALUES (?); INSERT INTO GiftCards (name) VALUES (?); INSERT INTO orders (price) VALUES (?)"*/;
+        var inserts = [req.body.email, req.body.name, req.body.amount];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/senders');
+            }
+        });
     });
 
 
