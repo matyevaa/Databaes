604beb7aef9935ca505662a7502c96ce043a210a
RedWolf625 <55816376+RedWolf625@users.noreply.github.com>
2020-08-08 12:25:02 -0700

    Update orderhistory.js

--- orderhistory.js
+++ orderhistory.js
@@ -4,30 +4,58 @@ module.exports = function(){
 
 
 
-    function getPeople(res, mysql, context, complete){
+    function getOrderHistory(res, mysql, context, complete){
         mysql.pool.query("SELECT OrderHistory.orderID as orderID, orders.trackerID as trackerID, OrderHistory.sender_email as sender_email, orders.recipient_email as recipient_email, OrderHistory.quantity as amount, GiftCards.name as name FROM OrderHistory LEFT JOIN senders ON OrderHistory.sender_email = senders.sender_email LEFT JOIN orders ON orders.orderID = OrderHistory.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.people = results;
+            context.OrderHistory = results;
             complete();
         });
     }
-    /*Display all people. Requires web based javascript to delete users with AJAX*/
+
+    /* Find OrderHistory whose fname starts with a given string in the req */
+    function getOrderHistoryByOrderID(req, res, mysql, context, complete) {
+      //sanitize the input as well as include the % character
+       var query = "SELECT OrderHistory.orderID as orderID, orders.trackerID as trackerID, OrderHistory.sender_email as sender_email, orders.recipient_email as recipient_email, OrderHistory.quantity as amount, GiftCards.name as name FROM OrderHistory LEFT JOIN senders ON OrderHistory.sender_email = senders.sender_email LEFT JOIN orders ON orders.orderID = OrderHistory.orderID LEFT JOIN GiftCards ON GiftCards.giftCardID = orders.giftCardID WHERE OrderHistory.orderID LIKE " + mysql.pool.escape(req.params.s + '%');
+      mysql.pool.query(query, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.OrderHistory = results;
+            complete();
+        });
+    }
+    /*Display all OrderHistory. Requires web based javascript to delete users with AJAX*/
 
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
-        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        context.jsscripts = ["search_orderhistory.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
+        getOrderHistory(res, mysql, context, complete);
         function complete(){
             callbackCount++;
             if(callbackCount >= 1){
                 res.render('orderhistory', context);
             }
 
+        }
+    });
+
+    router.get('/search/:s', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["search_orderhistory.js"];
+        var mysql = req.app.get('mysql');
+        getOrderHistoryByOrderID(req, res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('orderhistory', context);
+            }
         }
     });
 
