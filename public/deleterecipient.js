a46be4616371577bcc58fc4141576a404f9d1e8a
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-12 16:55:15 -0700

    Delete deleterecipient.js

--- public/deleterecipient.js
+++
@@ -1,9 +0,0 @@
-function deleteRecipient(id){
-    $.ajax({
-        url: '/recipients/' + id,
-        type: 'DELETE',
-        success: function(result){
-            window.location.reload(true);
-        }
-    })
-};
