2b7dfc174df15ae66c2ecd5ffb8a07555fad1509
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-12 16:55:23 -0700

    Delete deletesender.js

--- public/deletesender.js
+++
@@ -1,9 +0,0 @@
-function deleteSender(id){
-    $.ajax({
-        url: '/senders/' + id,
-        type: 'DELETE',
-        success: function(result){
-            window.location.reload(true);
-        }
-    })
-};
