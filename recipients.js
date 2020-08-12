bb26f7056a32e9e0bc00627167a1f1759d2edf2d
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-12 16:06:32 -0700

    Update recipients.js

--- recipients.js
+++ recipients.js
@@ -35,7 +35,7 @@ module.exports = function(){
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
-        context.jsscripts = ["search_recipients.js"];
+        context.jsscripts = ["deleterecipient.js","search_recipients.js"];
         var mysql = req.app.get('mysql');
         getPeople(res, mysql, context, complete);
         function complete(){
@@ -61,6 +61,22 @@ module.exports = function(){
         }
     });
 
+     /*Deletes a recipient, gives error 202 after a successful deleting*/
+     router.delete('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM recipients WHERE recipientID=?";
+        var inserts = [req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
     
 
     return router;
