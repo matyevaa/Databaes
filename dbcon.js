9058d16abcb48bc2d627d8915c733838daf46c07
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-08-12 17:28:11 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_matyevaa',
-  password        : '1452',
-  database        : 'cs340_matyevaa'
+  user            : '[cs340_yourengrusername]',
+  password        : '[password]',
+  database        : '[cs340_yourengrusername]'
 });
 
 module.exports.pool = pool;
