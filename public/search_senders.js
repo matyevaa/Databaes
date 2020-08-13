ece1f7db7bbf361ed5c14f11d441f91c7b6fc999
RedWolf625 <55816376+RedWolf625@users.noreply.github.com>
2020-08-12 17:33:35 -0700

    Update search_senders.js

--- public/search_senders.js
+++ public/search_senders.js
@@ -1,3 +1,4 @@
+//runs the select query for the senders table by passing the user inputted value to the /senders/search page
 function search_senders() {
     //get the first name 
     var senders_search_string  = document.getElementById('senders_search_string').value
